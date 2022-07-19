ca0e6c371b6decb4cddf5d43ac2d4f31d9fefbd7
ademola <ademolaowoeye07@gmail.com>
2022-07-19 14:03:31 +0100

    fixed the linters

--- app.js
+++ app.js
@@ -1,77 +1,61 @@
 function switching() {
+  const page = document.getElementsByClassName('real-page');
+  const pop = document.getElementsByClassName('pop-up');
+  const hambur1 = document.getElementsByClassName('nav1');
+  const hambur2 = document.getElementsByClassName('nav2');
+  const portf = document.getElementsByClassName('port');
+  const about = document.getElementsByClassName('abou');
+  const contac = document.getElementsByClassName('con');
 
-      const page = document.getElementsByClassName("real-page");
-      const pop = document.getElementsByClassName("pop-up")
-      const hambur1 = document.getElementsByClassName("nav1");
-      const hambur2 = document.getElementsByClassName("nav2");
+  hambur1[0].addEventListener('click', () => {
+    for (let i = 0; i < page.length; i += 1) {
+      page[i].style.display = 'none';
 
-      
+      for (let i = 0; i < pop.length; i += 1) {
+        pop[i].style.display = 'grid';
+      }
+    }
+  });
 
-       hambur1[0].addEventListener('click', function() {    
+  hambur2[0].addEventListener('click', () => {
+    for (let i = 0; i < page.length; i += 1) {
+      pop[i].style.display = 'none';
 
-        for (var i = 0; i < page.length; i++ ) {
-            page[i].style.display = "none"
+      for (let i = 0; i < pop.length; i += 1) {
+        page[i].style.display = 'grid';
+      }
+    }
+  });
 
-            for (var i = 0; i < pop.length; i++ ) {
-                pop[i].style.display = "grid"
-            }
-          }
-        }) 
+  portf[0].addEventListener('click', () => {
+    for (let i = 0; i < pop.length; i += 1) {
+      page[i].style.display = 'grid';
 
-        hambur2[0].addEventListener('click', function() { 
-    
-            for (var i = 0; i < page.length; i++ ) {
-                pop[i].style.display = "none"
-    
-                for (var i = 0; i < pop.length; i++ ) {
-                    page[i].style.display = "grid"
-                }
-              }
-            })
-            
-            
-            const portf = document.getElementsByClassName("port")
-           
-            portf[0].addEventListener('click', function() {
-                console.log('love!!!')      
-        
-                    for (var i = 0; i < pop.length; i++ ) {
-                        page[i].style.display = "grid"
+      for (let i = 0; i < page.length; i += 1) {
+        pop[i].style.display = 'none';
+      }
+    }
+  });
 
-                      for (var i = 0; i < page.length; i++ ) {
-                       pop[i].style.display = "none"
-                     }
-                  }
-                })
+  about[0].addEventListener('click', () => {
+    for (let i = 0; i < pop.length; i += 1) {
+      page[i].style.display = 'grid';
 
-              const about = document.getElementsByClassName("abou")
+      for (let i = 0; i < page.length; i += 1) {
+        pop[i].style.display = 'none';
+      }
+    }
+  });
 
-                about[0].addEventListener('click', function() {
-                    console.log('love!!!')      
-            
-                        for (var i = 0; i < pop.length; i++ ) {
-                            page[i].style.display = "grid"
-    
-                          for (var i = 0; i < page.length; i++ ) {
-                           pop[i].style.display = "none"
-                         }
-                      }
-                    })
-                    
-                    const contac = document.getElementsByClassName("con")
+  contac[0].addEventListener('click', () => {
+    for (let i = 0; i < pop.length; i += 1) {
+      page[i].style.display = 'grid';
 
-                    contac[0].addEventListener('click', function() {
-                        console.log('love!!!')      
-                
-                            for (var i = 0; i < pop.length; i++ ) {
-                                page[i].style.display = "grid"
-        
-                              for (var i = 0; i < page.length; i++ ) {
-                               pop[i].style.display = "none"
-                             }
-                          }
-                        })
- }
+      for (let i = 0; i < page.length; i += 1) {
+        pop[i].style.display = 'none';
+      }
+    }
+  });
+}
 
- switching();
-       
+switching();
