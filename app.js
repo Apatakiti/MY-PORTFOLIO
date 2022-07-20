1ee795c664d19d2ee64277754140c5a0302d7dab
ademola <ademolaowoeye07@gmail.com>
2022-07-20 20:51:06 +0100

    test my code in this branch

--- app.js
+++ app.js
@@ -1,61 +1,107 @@
-function switching() {
-  const page = document.getElementsByClassName('real-page');
-  const pop = document.getElementsByClassName('pop-up');
-  const hambur1 = document.getElementsByClassName('nav1');
-  const hambur2 = document.getElementsByClassName('nav2');
-  const portf = document.getElementsByClassName('port');
-  const about = document.getElementsByClassName('abou');
-  const contac = document.getElementsByClassName('con');
 
-  hambur1[0].addEventListener('click', () => {
-    for (let i = 0; i < page.length; i += 1) {
-      page[i].style.display = 'none';
 
-      for (let i = 0; i < pop.length; i += 1) {
-        pop[i].style.display = 'grid';
-      }
+const projbtn = document.getElementsByClassName('btn');
+const thePop = document.getElementsByClassName('the-pop-up')
+const blurs = document.getElementById('blur')
+const xmark = document.getElementById('x-mark')
+
+projbtn[0].addEventListener('click', function() {
+    console.log('me!!!')
+
+    for (let i = 0; i < thePop.length; i += 1) {
+          thePop[i].style.display = 'grid';    
     }
-  });
+    
+    blurs.style.filter = "blur(800px)";
+})
 
-  hambur2[0].addEventListener('click', () => {
-    for (let i = 0; i < page.length; i += 1) {
-      pop[i].style.display = 'none';
+xmark.addEventListener('click', function() {
+    for (let i = 0; i < thePop.length; i += 1) {
+        thePop[i].style.display = 'none';    
+  }
 
-      for (let i = 0; i < pop.length; i += 1) {
-        page[i].style.display = 'grid';
-      }
-    }
-  });
+  blurs.style.filter = "blur(0px)"
+})
 
-  portf[0].addEventListener('click', () => {
-    for (let i = 0; i < pop.length; i += 1) {
-      page[i].style.display = 'grid';
 
-      for (let i = 0; i < page.length; i += 1) {
-        pop[i].style.display = 'none';
-      }
-    }
-  });
 
-  about[0].addEventListener('click', () => {
-    for (let i = 0; i < pop.length; i += 1) {
-      page[i].style.display = 'grid';
 
-      for (let i = 0; i < page.length; i += 1) {
-        pop[i].style.display = 'none';
-      }
-    }
-  });
 
-  contac[0].addEventListener('click', () => {
-    for (let i = 0; i < pop.length; i += 1) {
-      page[i].style.display = 'grid';
 
-      for (let i = 0; i < page.length; i += 1) {
-        pop[i].style.display = 'none';
-      }
-    }
-  });
-}
 
-switching();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function switching() {
+//   const page = document.getElementsByClassName('real-page');
+//   const pop = document.getElementsByClassName('pop-up');
+//   const hambur1 = document.getElementsByClassName('nav1');
+//   const hambur2 = document.getElementsByClassName('nav2');
+//   const portf = document.getElementsByClassName('port');
+//   const about = document.getElementsByClassName('abou');
+//   const contac = document.getElementsByClassName('con');
+
+//   hambur1[0].addEventListener('click', () => {
+//     for (let i = 0; i < page.length; i += 1) {
+//       page[i].style.display = 'none';
+
+//       for (let i = 0; i < pop.length; i += 1) {
+//         pop[i].style.display = 'grid';
+//       }
+//     }
+//   });
+
+//   hambur2[0].addEventListener('click', () => {
+//     for (let i = 0; i < page.length; i += 1) {
+//       pop[i].style.display = 'none';
+
+//       for (let i = 0; i < pop.length; i += 1) {
+//         page[i].style.display = 'grid';
+//       }
+//     }
+//   });
+
+//   portf[0].addEventListener('click', () => {
+//     for (let i = 0; i < pop.length; i += 1) {
+//       page[i].style.display = 'grid';
+
+//       for (let i = 0; i < page.length; i += 1) {
+//         pop[i].style.display = 'none';
+//       }
+//     }
+//   });
+
+//   about[0].addEventListener('click', () => {
+//     for (let i = 0; i < pop.length; i += 1) {
+//       page[i].style.display = 'grid';
+
+//       for (let i = 0; i < page.length; i += 1) {
+//         pop[i].style.display = 'none';
+//       }
+//     }
+//   });
+
+//   contac[0].addEventListener('click', () => {
+//     for (let i = 0; i < pop.length; i += 1) {
+//       page[i].style.display = 'grid';
+
+//       for (let i = 0; i < page.length; i += 1) {
+//         pop[i].style.display = 'none';
+//       }
+//     }
+//   });
+// }
+
+// switching();
